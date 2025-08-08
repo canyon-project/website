c13ba9ac66041295c266760a9273e5b30ca8a640
Travis Zhang <wr.zhang25@gmail.com>
2025-08-08 13:53:17 +0800

    chore: daily development

--- content/cn/_meta.js
+++ content/cn/_meta.js
@@ -1,33 +1,18 @@
 export default {
   index: {
-    type: "page",
-    display: "hidden",
-    title: "Introduction",
+    type: 'page',
+    display: 'hidden',
     theme: {
-      // sidebar: true,
-      toc: false,
-    },
+      typesetting: 'article',
+      toc: false
+    }
   },
-  documentation: {
-    title: "文档",
-    type: "page",
+  docs: {
+    type: 'page',
+    title: '文档'
   },
   support: {
     title: "支持",
     type: "page",
   },
-  language: {
-    title: "语言",
-    type: "menu",
-    items: {
-      en: {
-        title: "English",
-        href: "https://docs.canyonjs.org",
-      },
-      ja: {
-        title: "日本語",
-        href: "https://ja.docs.canyonjs.org",
-      },
-    },
-  },
-};
+}
